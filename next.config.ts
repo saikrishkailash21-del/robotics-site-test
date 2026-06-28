77894070dd4538020774169fb8a6bdd6e607010b
Sai Krish <saikrishkailash21@gmail.com>
2026-06-28 17:19:35 +0530

    made small change

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "",
+  basePath: "/robotics-site-test", // <-- Change this from "" to your repository name
+  assetPrefix: "/robotics-site-test/", // <-- Add this line so Next.js prefixes your media paths
   images: {
     unoptimized: true,
     remotePatterns: [
