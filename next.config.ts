d58165fb9263b71dd76b97773ceca93891d2ec15
Sai Krish <saikrishkailash21@gmail.com>
2026-06-29 14:35:01 +0530

    chnage

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/robotics-site-test", 
+  basePath: "/robotics-site-test",
+  assetPrefix: "/robotics-site-test", // This forces Next.js to inject the prefix into assets!
   images: {
     unoptimized: true,
     remotePatterns: [
